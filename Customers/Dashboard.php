91e5448fea7daef285d1932de8465cfe216388dc
Adika Muhammad Haikal <haikaladika272@gmail.com>
2025-05-20 11:05:22 +0700

    Update Customers

---
+++ Customers/Dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GEMS - Beranda</title>
+</head>
+<body>
+
+<h2>Selamat datang di Beranda </h2>
+
+    <form method="GET" action="shop.php">
+        <input type="submit" value="Shop"> 
+    </form>
+
+    <form method="GET" action="account.php">
+        <input type="submit" value="Akun saya">
+    </form>
+
+    <form method="GET" action="logout.php">
+        <input type="submit" value="Logout">
+    </form>
+</body>
+</html>
