483713edcb189341b422e98fcbd9f2903774ba47
Alifi <alifibakhsi89@gmail.com>
2025-05-27 11:54:32 +0700

    navbar update

--- UI/gems-customer-pages/shop.php
+++ UI/gems-customer-pages/shop.php
@@ -171,8 +171,8 @@
 
 				<div class="breadcrumbs d-flex flex-row align-items-center">
 					<ul>
-						<li><a href="index.html">Home</a></li>
-						<li class="active"><a href="index.html"><i class="fa fa-angle-right" aria-hidden="true"></i>Men's</a></li>
+						<li><a href="dashboard.php">Home</a></li>
+						<li><a href="shop.php"><i></i>shop</a></li>
 					</ul>
 				</div>
 
