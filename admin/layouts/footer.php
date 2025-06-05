01dd7c12e2b7e62f7c5dfad912fba8e969b8c1ee
Adika Muhammad Haikal <haikaladika272@gmail.com>
2025-06-05 10:17:25 +0700

    update admin

--- admin/layouts/footer.php
+++ admin/layouts/footer.php
@@ -3,7 +3,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; 2022 | Sistem Informasi Itenas Bandung | This website is made with <i class="fas fa-heart" aria-hidden="true" style="color: red;"></i> by <a href="https://github.com/kurniaramadhan" target="_blank">Kurnia Ramadhan Putra</a></span>
+            <span>Copyright &copy; 2025 | GEMS STORE | This website is made with <i class="fas fa-gem" aria-hidden="true" style="color: blue;"></i> by <a href="https://github.com/kurniaramadhan" target="_blank">GEVAM Reborn</a></span>
         </div>
     </div>
 </footer>
